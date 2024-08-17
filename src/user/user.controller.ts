41e75c9d10955e12e6201bda230bd7e8719e9c61
alexanderthinh <alexanderthinh@gmail.com>
2024-08-17 23:54:04 +0700

    Custom param decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,32 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Put, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
+import { GetUser, Roles } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
+import { UserResponse } from 'src/response/reponse.user';
+import { UpdateUserRoleRequest } from './dto/user.dto';
+import { UserService } from './user.service';
+import { UserRole } from 'src/enum/user-role';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-    @UseGuards(JwtGuard)
+    constructor(private userService: UserService) {}
+
     @Get('me')
-    getMe(@Req() req: Request) {
+    getMe(@GetUser() user: UserResponse) {
         // When use @UseGuards(JwtGuard), the req is received from method validate of class JwtStrategy
         //  and req.user is a default of application
-        return req.user; 
+        // return req.user; 
+
+        return user; 
+    }
+
+    @Roles(UserRole.ADMIN)
+    @Put('user-role')
+    async updateUserRole(@GetUser() currentUser: UserResponse, @Body() updateUserRoleRequest: UpdateUserRoleRequest) {
+        // console.log('currentUser', currentUser);
+        
+        const userResponse = await this.userService.updateUserRole(updateUserRoleRequest);
+        return userResponse;
     }
 }
